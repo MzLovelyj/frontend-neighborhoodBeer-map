16ebd40969c0f8af5c8bc841c06675252b44cd9c
mzlovelyj <liljp064@gmail.com>
2018-11-11 10:44:41 -0500

    Updates

---
+++ precache-manifest.7bd611804e19f1d7fddcdb3b768384ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7dda5140f3cd2f2fe545fe6affc9faa",
+    "url": "/frontend-neighborhoodBeer-map/static/media/foursquare.e7dda514.png"
+  },
+  {
+    "revision": "1cbd2e082cd207fc206418d0a4b17a5b",
+    "url": "/frontend-neighborhoodBeer-map/static/media/circles-loader.1cbd2e08.svg"
+  },
+  {
+    "revision": "0069d7e769da43b0dbb9",
+    "url": "/frontend-neighborhoodBeer-map/static/js/runtime~main.0069d7e7.js"
+  },
+  {
+    "revision": "0fceaa8bf66405aed8bf",
+    "url": "/frontend-neighborhoodBeer-map/static/js/main.0fceaa8b.chunk.js"
+  },
+  {
+    "revision": "845a235301a9671c6917",
+    "url": "/frontend-neighborhoodBeer-map/static/js/1.845a2353.chunk.js"
+  },
+  {
+    "revision": "0fceaa8bf66405aed8bf",
+    "url": "/frontend-neighborhoodBeer-map/static/css/main.726fc8fc.chunk.css"
+  },
+  {
+    "revision": "f242fa999b5852dd7177b9fef80ec601",
+    "url": "/frontend-neighborhoodBeer-map/index.html"
+  }
+];
